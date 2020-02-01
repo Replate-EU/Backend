a7d3e75e6c85f3ab057e51718625004b46e82b54
Darragh Ferry <darraghferry@hotmail.com>
2020-02-01 12:55:50 +0000

    added routers to server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const apiRouter = require("./api-router");
+const authRouter = require("../routers/auth-router");
+const pickupRouter = require("../routers/pickups-router");
+const userRouter = require("../routers/users-router");
 
 const server = express();
+
+server.use("/auth", authRouter);
+server.use("/pickups", pickupRouter);
+server.use("/users", userRouter);
 
 server.use("/api", apiRouter);
 
