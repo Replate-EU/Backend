1e2904b2afcac7ca7d539b6886f82dc5097352a4
Darragh Ferry <darraghferry@hotmail.com>
2020-02-01 14:31:33 +0000

    fixed problems with server/api-router

--- api/api-router.js
+++ api/api-router.js
@@ -1,5 +1,15 @@
 const router = require("express").Router();
 
+const authRouter = require("../routers/auth-router");
+const pickupRouter = require("../routers/pickups-router");
+const userRouter = require("../routers/users-router");
+
+const checkToken = require("../middleware/checkToken");
+
+router.use("/auth", authRouter);
+router.use("/pickups", checkToken, pickupRouter);
+router.use("/users", checkToken, userRouter);
+
 router.get("/", (req, res) => {
   res.json("Server is Running.");
 });
--- api/server.js
+++ api/server.js
@@ -1,17 +1,8 @@
 const express = require("express");
 
 const apiRouter = require("./api-router");
-const authRouter = require("../routers/auth-router");
-const pickupRouter = require("../routers/pickups-router");
-const userRouter = require("../routers/users-router");
-
-const checkToken = require("../middleware/checkToken");
 
 const server = express();
-
-server.use("/auth", authRouter);
-server.use("/pickups", checkToken, pickupRouter);
-server.use("/users", checkToken, userRouter);
 
 server.use("/api", apiRouter);
 
