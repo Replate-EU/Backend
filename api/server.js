658f3a7d99c6490fa6a03a08fa34b12762fd4208
Darragh Ferry <darraghferry@hotmail.com>
2020-02-01 13:00:31 +0000

    added checkToken middleware to pickup and user router

--- api/server.js
+++ api/server.js
@@ -1,14 +1,17 @@
 const express = require("express");
+
 const apiRouter = require("./api-router");
 const authRouter = require("../routers/auth-router");
 const pickupRouter = require("../routers/pickups-router");
 const userRouter = require("../routers/users-router");
 
+const checkToken = require("../middleware/checkToken");
+
 const server = express();
 
 server.use("/auth", authRouter);
-server.use("/pickups", pickupRouter);
-server.use("/users", userRouter);
+server.use("/pickups", checkToken, pickupRouter);
+server.use("/users", checkToken, userRouter);
 
 server.use("/api", apiRouter);
 
