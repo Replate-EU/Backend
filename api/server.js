713cc1295327a1809fba9b938c197edab16d9534
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-02-02 17:21:16 +0500

    Plugged in cors and helmet

--- api/server.js
+++ api/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const cors = require('cors');
+const helmet = require('helmet');
+
 
 const apiRouter = require("./api-router");
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
+server.use(helmet());
 server.use("/api", apiRouter);
 
 module.exports = server;
